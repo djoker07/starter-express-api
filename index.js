923a9e8ffd14599e8f652271ab39a0c60f672a62
djoker07 <danielc718@gmail.com>
2022-12-12 16:27:56 -0500

    adding products

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+const port = 3000
+app.use(cors())
+
+const data = require('./products.json')
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000)
+app.get('/products', (req, res) => {
+    res.json(data)
+})
+
+app.listen(process.env.PORT || port)
